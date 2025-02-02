e4d3100b2df683acaa584a3ba53c5cd7ea84d21d
“sangmiiiiiin” <kwk680037@gmail.com>
2025-02-02 15:17:02 +0900

    💄 mainpage style 수정

--- frontend/src/styles/MainStyles.js
+++ frontend/src/styles/MainStyles.js
@@ -4,19 +4,19 @@ import styled from 'styled-components';
 export const CardContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);  // 3개의 열로 분할
-  gap: 20px;
-  padding: 20px;
+  gap: 20px;    // 각 카드 사이 (가로, 세로 모두)
+  padding: 20px;    // 컨테이너 안에 있는 태그들의 여백을 설정
 `;
 
 export const Card = styled.div`
-  border: 1px solid #ddd;
+  border: 1px solid #ddd;   // 테두리(연한 회색)
   border-radius: 8px;
   overflow: hidden;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  transition: transform 0.3s ease;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  transition: transform 0.4s ease;  // 대기 시간은 생략되어 있다
 
   &:hover {
-    transform: translateY(-10px);
+    transform: translateY(-5px);
   }
 `;
 
