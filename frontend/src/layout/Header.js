0e3e888bab387753300ca19e4908bc4bbf50339f
sangmiiiiin <kwk680037@gmail.com>
2025-02-16 17:29:14 +0900

    ✨ 🔥 💄 Header 컴포넌트 UI 수정 및 사이드바, 프로필 메뉴 추가

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,27 +1,183 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { HeaderContainer, Logo, NavList, NavLink } from '../styles/HeaderStyles';  // 스타일 임포트
+// import { Link } from 'react-router-dom';
+// import { HeaderContainer, Logo, NavList, NavLink } from '../styles/HeaderStyles';  // 스타일 임포트
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
+
+import HeaderDrawer from '../components/HeaderDrawer';
+
+const pages = ['Guestbook', 'Home', 'Login'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   return (
-    <HeaderContainer>
-      <Logo>
-        <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>Sangmin's Blog</Link>
-      </Logo>
-      <nav>
-        <NavList>
-          <li>
-            <NavLink href="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink href="/guestbook">Guestbook</NavLink>
-          </li>
-          <li>
-            <NavLink href="/about">About</NavLink>
-          </li>
-        </NavList>
-      </nav>
-    </HeaderContainer>
+    // 죽은 코드
+    // <HeaderContainer>
+    //   <Logo>
+    //     <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>Sangmin's Blog</Link>
+    //   </Logo>
+    //   <nav>
+    //     <NavList>
+    //       <li>
+    //         <NavLink href="/">Home</NavLink>
+    //       </li>
+    //       <li>
+    //         <NavLink href="/guestbook">Guestbook</NavLink>
+    //       </li>
+    //       <li>
+    //         <NavLink href="/about">About</NavLink>
+    //       </li>
+    //     </NavList>
+    //   </nav>
+    // </HeaderContainer>
+
+    <AppBar position="static" sx={{ backgroundColor: '#333'}}>
+    <Container maxWidth="xl">
+      <Toolbar disableGutters>
+        <HeaderDrawer />
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'inherit',
+            textDecoration: 'none',
+          }}
+        >
+          Sangmin's Blog
+        </Typography>
+
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{ display: { xs: 'block', md: 'none' } }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+        <DynamicFeedIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+        <Typography
+          variant="h5"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            display: { xs: 'flex', md: 'none' },
+            flexGrow: 1,
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'inherit',
+            textDecoration: 'none',
+          }}
+        >
+          Sangmin's Blog
+        </Typography>
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: 'white', display: 'black' }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </Container>
+  </AppBar>
   );
 }
 
