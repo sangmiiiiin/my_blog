3c29963c4442960dd5dc40e63e44297d5222b8d8
“sangmiiiiiin” <kwk680037@gmail.com>
2025-02-17 23:55:58 +0900

    📱 💄  🔥 모바일 환경에서 헤더에 새글작성 추가, Create 페이지 UI 수정, 모바일 환경에서 메인페이지 반응형 디자인 수정, 불필요한 스타일드 컴포넌트 제거

--- frontend/src/styles/CreateStyles.js
+++ frontend/src/styles/CreateStyles.js
@@ -4,18 +4,7 @@ export const NewPostContainer = styled.div`
   display: grid;
 `;
 
-export const NewPostTitle = styled.input`
-  width: 80%;
-  height: 5vh;
-  margin-bottom: 10px;
-`;
-
-export const Textarea = styled.textarea`
-  width: 80%;
-  height: 50vh;
-`;
-
 export const SaveButton = styled.button`
   margin-top: 10px;
   width: 10%;
-`
+`;
