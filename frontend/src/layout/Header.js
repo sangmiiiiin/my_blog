5d514c56459cdde7d8a2f1b0ee39e0867c8eea3d
sangmiiiiin <kwk680037@gmail.com>
2025-02-19 00:23:20 +0900

    🔥 Header에 사용하지 않는 컴포넌트 import문 제거

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -8,13 +8,11 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
 
 import HeaderDrawer from '../components/HeaderDrawer';
 import CreateButton from '../components/CreateButton';
