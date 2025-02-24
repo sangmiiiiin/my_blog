575dd083197b2a9e993ff5a85ed57b8159305e26
“sangmiiiiiin” <kwk680037@gmail.com>
2025-02-25 00:01:22 +0900

    🎨  backend 로직 파일 구조 변경

---
+++ backend/config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+
+// MongoDB 연결
+// mongoose.connect("mongodb://localhost:27017/blogDB", {
+//           useNewUrlParser: true,   
+//           useUnifiedTopology: true 
+// }).then(() => console.log("✅ MongoDB 연결 성공!"))
+// .catch(err => console.error("❌ MongoDB 연결 실패:", err));
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/blogDB", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("✅ MongoDB 연결 성공!");
+    } catch (err) {
+        console.error("❌ MongoDB 연결 실패:", err);
+    }
+};
+
+module.exports = connectToDatabase;
