6d7dbbeb016999057071909a07a65ecf3f85e7ec
sangmiiiiin <kwk680037@gmail.com>
2025-02-24 23:33:19 +0900

    💄 상세보기 창에도 이미지가 보이게 수정, 글 수정 화면 UI 수정

--- frontend/src/pages/Edit.js
+++ frontend/src/pages/Edit.js
@@ -82,28 +82,26 @@
 // export default Edit;
 
 // frontend/src/pages/Edit.js
-import { Box, Button, Card, CardContent, Paper, TextField, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, CardMedia, Paper, TextField, Typography } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 const Edit = () => {
-    // const [post, setPost] = useState({
-    //     title: "",
-    //     content: ""
-    // });
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
+    const [img, setImg] = useState(null);
     const { id } = useParams();
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchPost = async () => {
             try {
-                const response = await axios.get(`http://192.168.10.100:5700/posts/${id}`);
+                const response = await axios.get(`http://192.168.10.102:5700/posts/${id}`);
                 // setPost(response.data);
                 setTitle(response.data.title);
                 setContent(response.data.content);
+                setImg(response.data.thumbnail);
             } catch(error) {
                 console.error("글 조회 오류 발생: ", error);
             }
@@ -114,7 +112,7 @@ const Edit = () => {
     const handleEdit = async (e) => {
         e.preventDefault();
         try {
-            await axios.put(`http://192.168.10.100:5700/posts/${id}`, { title, content });
+            await axios.put(`http://192.168.10.102:5700/posts/${id}`, { title, content });
             navigate(`/posts/${id}`);
         } catch(error) {
             console.error("수정 중 오류가 발생했습니다: ", error);
@@ -127,7 +125,17 @@ const Edit = () => {
                 <Typography variant="h5" gutterBottom>
                     글 수정
                 </Typography>
-                <Card variant="outlined">
+                <Card variant="outlined"
+                    sx={{
+                        width: "100%",
+                        objectFit: "cover",
+                        height: "auto"
+                    }}>
+                    <CardMedia
+                        component="img"
+                        image={img}
+                        alt="Thumnail Image..."
+                        />
                     <CardContent>
                         <form onSubmit={handleEdit}>
                             <TextField
