367ee20907c665c3b297c3376db9dd4c856f093f
“sangmiiiiiin” <kwk680037@gmail.com>
2025-02-28 00:09:40 +0900

    🎨 이미지 업로드 컨트롤러와 라우터 분리

---
+++ backend/routes/uploadRoute_20250228_bak.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const router = express.Router();
+
+// 저장 경로 설정
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/"); // 이미지 저장 폴더
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)); // 파일명 중복 방지
+    },
+});
+
+const upload = multer({ storage });
+
+// 이미지 업로드 API
+router.post("/", upload.single("image"), (req, res) => {
+    console.log("📌 요청 도착: /upload");
+    console.log("📌 업로드된 파일 정보:", req.file);
+
+    if (!req.file) {
+        console.error("❌ 파일 없음");
+        return res.status(400).json({ message: "이미지 업로드 실패"});
+    }
+    res.json({ imageUrl: `http://192.168.10.102:5700/public/${req.file.filename}` });
+});
+
+module.exports = router;
