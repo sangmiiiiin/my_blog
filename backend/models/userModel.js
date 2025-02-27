65800cc3b69569928d6c315372ba8944259bf906
“sangmiiiiiin” <kwk680037@gmail.com>
2025-02-28 00:46:55 +0900

    👔 회원가입 API 및 로직 추가

---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createAt: { type: Date, default: Date.now },
+});
+
+// 비밀번호 해싱 (회원가입시 자동 적용)
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
