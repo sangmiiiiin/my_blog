4182f6466ff74fa3ef69fdf3a68658e607838d5c
sangmiiiiin <kwk680037@gmail.com>
2025-03-04 23:56:24 +0900

    🎨  server.js에서 import 하던 각 routes를 모듈화

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,7 @@ const cors = require("cors");
 const path = require("path");
 
 const connectToDatabase = require("./config/db");
-
-const uploadRoutes = require("./routes/uploadRoute");
-const postRoutes = require("./routes/postRoutes");
-const authRoutes = require("./routes/authRoutes");
+const routes = require("./routes/index");
 
 dotenv.config();
 connectToDatabase();  // MongoDB 연결
@@ -16,11 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/posts", postRoutes);  // 블로그 API 엔드포인트
-app.use("/auth", authRoutes); // 회원가입 & 로그인 API 추가
-app.use("/upload", uploadRoutes);  // 업로드 API 추가
-app.use("/public", express.static("public"));  // 정적 파일 제공
+// ✅ 라우트 통합 적용
+app.use("/", routes); 
 
+app.use("/public", express.static("public"));  // 정적 파일 제공
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
