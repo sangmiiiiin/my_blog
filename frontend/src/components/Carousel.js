809ce4768185078f406ad1b94256010992b16e5d
“sangmiiiiiin” <kwk680037@gmail.com>
2025-03-12 23:16:00 +0900

    💄 ✨ Carousel 추가

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -1,58 +1,45 @@
-import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Box from '@mui/joy/Box';
-import Typography from '@mui/joy/Typography';
-import Card from '@mui/joy/Card';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css"; 
+import { Card, CardMedia, CardContent, Typography } from "@mui/material";
 
-const data = [
-  {
-    src: 'https://images.unsplash.com/photo-1502657877623-f66bf489d236',
-    title: 'Night view',
-    description: '4.21M views',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1527549993586-dff825b37782',
-    title: 'Lake view',
-    description: '4.74M views',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36',
-    title: 'Mountain view',
-    description: '3.98M views',
-  },
-];
+import image1 from "../image/clothes-1.jpg";
+import image2 from "../image/clothes-2.jpg";
+import image3 from "../image/clothes-3.jpg";
 
-export default function CarouselRatio() {
+const MUICarousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000
+  };
+
+  const items = [
+    { title: "클라이밍 바지", image: image1 },
+    { title: "클라이밍 상의", image: image2 },
+    { title: "초크백", image: image3 }
+  ];
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        gap: 1,
-        py: 1,
-        overflow: 'auto',
-        width: 343,
-        scrollSnapType: 'x mandatory',
-        '& > *': {
-          scrollSnapAlign: 'center',
-        },
-        '::-webkit-scrollbar': { display: 'none' },
-      }}
-    >
-      {data.map((item) => (
-        <Card orientation="horizontal" size="sm" key={item.title} variant="outlined">
-          <AspectRatio ratio="1" sx={{ minWidth: 60 }}>
-            <img
-              srcSet={`${item.src}?h=120&fit=crop&auto=format&dpr=2 2x`}
-              src={`${item.src}?h=120&fit=crop&auto=format`}
-              alt={item.title}
-            />
-          </AspectRatio>
-          <Box sx={{ whiteSpace: 'nowrap', mx: 1 }}>
-            <Typography level="title-md">{item.title}</Typography>
-            <Typography level="body-sm">{item.description}</Typography>
-          </Box>
-        </Card>
-      ))}
-    </Box>
+    <div style={{ width: "100%", margin: "auto" }}>
+      <Slider {...settings}>
+        {items.map((item, index) => (
+          <Card key={index}>
+            <CardMedia sx={{ objectFit: "fill" }} component="img" height="300" image={item.image} alt={item.title} />
+            <CardContent>
+              <Typography variant="h6">{item.title}</Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </Slider>
+    </div>
   );
-}
+};
+
+export default MUICarousel;
+
