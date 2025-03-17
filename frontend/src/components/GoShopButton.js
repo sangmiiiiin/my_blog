d9d99bb8d3ef70876f7b7fc3acb1b619410e02ba
“sangmiiiiiin” <kwk680037@gmail.com>
2025-03-17 22:51:40 +0900

    💄 웰컴페이지 수정

---
+++ frontend/src/components/GoShopButton.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function GoShopButton() {
+  return (
+      <Button variant="contained" href="/main" color="">
+        Go shopping!
+      </Button>
+  );
+}
