a8c5deda2279b48ce44425c81a3c6b510a45880b
sangmiiiiin <kwk680037@gmail.com>
2025-03-23 22:35:06 +0900

    🎨 Redux-persist 예제

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducr from "./counterSlice";
 import userReducer from "./userSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage 사용
+
+// Redux Persist 설정
+const persistConfig = {
+    key: "root", // 저장될 키 값
+    storage, // localStorage에 저장
+};
+
+// Persist Reducer 생성
+const persistUserReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
     reducer: {
         counter: counterReducr, // counter 상태 추가
-        users: userReducer,
+        users: persistUserReducer, // Redux Persist 적용된 Reducer 사용
     },
 });
 
+export const persistor = persistStore(store); // Redux Persist Store 생성
+
+export default store;
