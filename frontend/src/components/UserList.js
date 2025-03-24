0f1450c59dcaeea9c0ffeae97c00e7c01d905d4d
“sangmiiiiiin” <kwk680037@gmail.com>
2025-03-25 00:19:00 +0900

    💩 Httponly 쿠키 jwt token 보유시 redux로 상태 업데이트 > UI변경

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchUsers } from "../redux/userSlice";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor } from "../redux/store";
+// import React, { useEffect } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { fetchUsers } from "../redux/userSlice";
+// import { PersistGate } from "redux-persist/integration/react";
+// import { persistor } from "../redux/store";
 
-const UserList = () => {
-    const dispatch = useDispatch();
-    const { users, loading, error } = useSelector((state) => state.users);
+// const UserList = () => {
+//     const dispatch = useDispatch();
+//     const { users, loading, error } = useSelector((state) => state.users);
 
-    useEffect(() => {
-        dispatch(fetchUsers()); // API 요청 실행
-    }, [dispatch]);
+//     useEffect(() => {
+//         dispatch(fetchUsers()); // API 요청 실행
+//     }, [dispatch]);
 
-    if (loading) return <p>로딩 중...</p>;
-    if (error) return <p>에러 발생: {error}</p>;
+//     if (loading) return <p>로딩 중...</p>;
+//     if (error) return <p>에러 발생: {error}</p>;
 
-    return (
-        <PersistGate loading={<p>로딩 중...</p>} persistor={persistor}>
-            <div>
-                <h2>사용자 목록</h2>
-                <ul>
-                    {users.map((user) => (
-                        <li key={user.id}>{user.name}</li>
-                    ))}
-                </ul>
-            </div>
-        </PersistGate>
-    );
-};
+//     return (
+//         <PersistGate loading={<p>로딩 중...</p>} persistor={persistor}>
+//             <div>
+//                 <h2>사용자 목록</h2>
+//                 <ul>
+//                     {users.map((user) => (
+//                         <li key={user.id}>{user.name}</li>
+//                     ))}
+//                 </ul>
+//             </div>
+//         </PersistGate>
+//     );
+// };
 
-export default UserList;
+// export default UserList;
