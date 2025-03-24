23524e31506fc43b08c99d362e156e23f53c5784
“sangmiiiiiin” <kwk680037@gmail.com>
2025-03-24 22:30:19 +0900

    🔒️ 👔 🎨  로그인시 jwt token 발급, httponly 쿠키에 저장,확인로직

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -4,12 +4,14 @@ const verifyToken = (req, res, next) => {
     const token = req.cookies.token;
     if (!token) return res.status(401).json({ message: "인증 필요" });
 
-    jwt.verify(token, process.env.JWT_SECRET || "secretkey", (err, decoded) => {
-        if (err) return res.status(403).json({ message: "유효하지 않은 토큰" });
-
-        req.user = decoded;
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || "secretkey");
+        req.user = decoded; // 유저 정보 저장
         next();
-    });
+    } catch (error) {
+        return res.status(401).json({ isAuthenticated: false });
+    }
 };
+
 
 module.exports = verifyToken;
