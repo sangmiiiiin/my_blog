2b7fe314e1bab59ff396a6af84e6ec7df5522a2d
sangmiiiiin <kwk680037@gmail.com>
2025-03-24 22:30:19 +0900

    🔒️ 👔 🎨  로그인시 jwt token 발급, httponly 쿠키에 저장,확인로직

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { registerUser } = require("../controllers/authController");
 const { loginUser } = require("../controllers/loginController");
+const { authCheckUser } = require("../controllers/authCheckController");
+const verifyToken = require("../middlewares/verifyToken");
 
 const router = express.Router();
 
 router.post("/register", registerUser); // 회원가입 엔드포인트
 router.post("/login", loginUser); // 로그인 엔드포인트
+router.get("/check", verifyToken, authCheckUser); // 토큰확인 엔드포인트
 
 module.exports = router;
 
