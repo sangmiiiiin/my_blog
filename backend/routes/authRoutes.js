6baf030f7064377b9b01d75b99f6ce232d69191b
sangmiiiiin <kwk680037@gmail.com>
2025-03-26 23:08:58 +0900

    💄 ✨ 로그아웃 기능 추가

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,12 +3,14 @@ const { registerUser } = require("../controllers/authController");
 const { loginUser } = require("../controllers/loginController");
 const { authCheckUser } = require("../controllers/authCheckController");
 const verifyToken = require("../middlewares/verifyToken");
+const { logoutUser } = require("../controllers/logoutController");
 
 const router = express.Router();
 
 router.post("/register", registerUser); // 회원가입 엔드포인트
 router.post("/login", loginUser); // 로그인 엔드포인트
 router.get("/check", verifyToken, authCheckUser); // 토큰확인 엔드포인트
+router.post("/logout", logoutUser); // 로그아웃 엔드포인트
 
 module.exports = router;
 
