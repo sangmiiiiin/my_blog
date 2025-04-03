d98035b4103649c9e59ab936bffa840d249b71d5
sangmiiiiin <kwk680037@gmail.com>
2025-04-04 00:12:12 +0900

    💄 제품 상세 페이지 컴포넌트 추가 및 UI 수정

---
+++ frontend/src/components/Select.js
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { Box } from '@mui/material';
+
+const SizeSelect = () => {
+  const [size, setSize] = React.useState('');
+
+  const handleChange = (event) => {
+    setSize(event.target.value);
+  };
+
+  const sizelist = ["S", "M", "L"]
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 360 }}>
+        <InputLabel id="Size Selector">Size</InputLabel>
+        <Select
+          labelId="Size Selector"
+          id="Size Selector"
+          value={size}
+          label="Size"
+          onChange={handleChange}
+          sx={{ 
+            height: 40,
+            fontSize: "15px"
+          }}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={sizelist[0]}>S</MenuItem>
+          <MenuItem value={sizelist[1]}>M</MenuItem>
+          <MenuItem value={sizelist[2]}>L</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+const ColorSelect = () => {
+  const [color, setColor] = React.useState('');
+
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  };
+
+  const colorList = ["Black", "Navy", "Pink"];
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 360 }}>
+        <InputLabel id="Color Selector">Color</InputLabel>
+        <Select
+          id="Color Selector"
+          labelId="Color Selector"
+          value={color}
+          label="Color"
+          onChange={handleColorChange}
+          sx={{ 
+            height: 40,
+            fontSize: "15px",
+          }}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={colorList[0]}>Black</MenuItem>
+          <MenuItem value={colorList[1]}>Navy</MenuItem>
+          <MenuItem value={colorList[2]}>Pink</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+export default function OptionSelect() {
+  return (
+    <Box 
+      sx={{
+        mt: 2,
+      }}
+    >
+      <SizeSelect />
+      <ColorSelect />
+    </Box>
+  );
+}
