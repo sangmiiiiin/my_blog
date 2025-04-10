7c7e8aa49c6277aa55a0ad19015fa81cfb656173
sangmiiiiin <kwk680037@gmail.com>
2025-04-11 00:49:53 +0900

    💄 ✨ 🐛 상품 상세 페이지에서 Size와 Color 가 등록한대로 보이게 수정

--- frontend/src/pages/Detail.js
+++ frontend/src/pages/Detail.js
@@ -2,9 +2,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Card, CardMedia, CardContent, Typography, Button, Box, Paper, Divider } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography, Button, Box, Paper, Divider, Select, MenuItem, InputLabel, FormControl } from "@mui/material";
 import CarouselRatio from '../components/MobileCarousel';
-import OptionSelect from "../components/Select";
+// import OptionSelect from "../components/Select";
 import Price from "../components/Price";
 import OrderBox from "../components/OrderBox";
 import DetailSection from "../components/DetailSection";
@@ -18,6 +18,8 @@ const Detail = () => {
         salePrice: 49800,
         deliveryFee: 3000,
     }
+    const [size, setSize] = useState("");
+    const [color, setColor] = useState("");
 
     useEffect(() => {
         axios.get(`http://localhost:5700/posts/${id}`)
@@ -27,8 +29,9 @@ const Detail = () => {
 
     const handleDelete = async () => {
         try {
-            await axios.delete(`http://localhost:5700/posts/${id}`);
-            navigate("/", { state: { delete: true } });
+            const response = axios.delete(`http://localhost:5700/posts/${id}`);
+            console.log("삭제 성공:", response.data);
+            navigate("/main", { state: { delete: true } });
         } catch (error) {
             console.error("삭제 오류:", error);
         }
@@ -87,7 +90,27 @@ const Detail = () => {
                 </Box>
 
                 <Price originalPrice={post.originalPrice} discountedPrice={post.salePrice} deliveryFee={detailInformation.deliveryFee} />
-                <OptionSelect clothesSize={detailInformation.size} clothesColor={detailInformation.color}/>
+                {post.options?.sizes && (
+                    <FormControl sx={{ m: 1, minWidth: 360 }}>
+                        <InputLabel id="Size Selector" sx={{ lineHeight: 0.75 }}>Size</InputLabel>
+                        <Select label="size" fullWidth value={size} onChange={(e) => setSize(e.target.value)} sx={{ my: 0, height: 40 }}>
+                            {post.options.sizes.map((s) => (
+                                <MenuItem key={s} value={s}>{s}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                )}
+                {post.options?.colors && (
+                    <FormControl sx={{ m: 1, minWidth: 360 }}>
+                        <InputLabel id="Size Selector" sx={{ lineHeight: 0.75 }}>Color</InputLabel>
+                        <Select label="color" fullWidth value={color} onChange={(e) => setColor(e.target.value)} sx={{ height: 40 }}>
+                            {post.options.colors.map((c) => (
+                                <MenuItem key={c} value={c}>{c}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                )}
+                {/* <OptionSelect clothesSize={detailInformation.size} clothesColor={detailInformation.color} /> */}
 
                 <Box display="flex" justifyContent="center">
                     <Divider
@@ -100,7 +123,7 @@ const Detail = () => {
 
                 <OrderBox />
 
-                <DetailSection detailContent={post.detailContent}/>
+                <DetailSection detailContent={post.detailContent} />
 
                 <Box display="flex" justifyContent="space-between" p={2}>
                     <Button variant="contained" color="primary" onClick={() => navigate(`/edit/${id}`)}>수정</Button>
