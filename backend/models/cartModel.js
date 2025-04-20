d2c1bb5c8f5a20aba93d6d94eafa09f61f056025
sangmiiiiin <kwk680037@gmail.com>
2025-04-20 23:44:51 +0900

    🎨 장바구니 스키마 모듈 export 코드 수정

--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -15,4 +15,6 @@ const cartSchema = new mongoose.Schema({
   updatedAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("Cart", cartSchema);
+const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = Cart;
