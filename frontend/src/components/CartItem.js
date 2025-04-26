7169a987d4f69481e39106a12742a705bb840da0
sangmiiiiin <kwk680037@gmail.com>
2025-04-26 22:23:54 +0900

    ✨ MAP 함수로 장바구니에 백엔드에서 받아온 데이터 화면 출력

--- frontend/src/components/CartItem.js
+++ frontend/src/components/CartItem.js
@@ -1,51 +1,63 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Button, Card, CardContent, CardMedia, Typography, IconButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import axios from "axios";
 
 export default function CartItem({ item, onIncrease, onDecrease, onRemove }) {
   const { image, name, color, size, price, quantity } = item;
-  useEffect(async () => {
-    try {
-      const response = await axios.get("http://localhost:5700/cart/show",
-        { withCredentials: true } // 쿠키포함
-      );
-      console.log("불러오기 성공:", response.data);
-    } catch (error) {
-      console.error("장바구니 불러오기 오류:", error.response ? error.response.data : error);
+  const [carts, setCarts] = useState([]);
+
+  useEffect(() => {
+    const getCart = async () => {
+      try {
+        const response = await axios.get("http://localhost:5700/cart/show",
+          { withCredentials: true } // 쿠키포함
+        );
+        console.log("불러오기 성공:", response.data);
+        console.log("썸네일 URL:", response.data.items[0].product.thumbnail)
+        setCarts(response.data.items)
+      } catch (error) {
+        console.error("장바구니 불러오기 오류:", error.response ? error.response.data : error);
+      }
     }
+    getCart();
   }, [])
+
   return (
-    <Card sx={{ display: "flex", mb: 2, p: 1 }}>
-      <CardMedia
-        component="img"
-        image={image}
-        alt={name}
-        sx={{ width: 100, height: 100, borderRadius: 2 }}
-      />
+    <>
+      {carts.map(cart => (
+        <Card sx={{ display: "flex", mb: 2, p: 1 }}>
+          <CardMedia
+            component="img"
+            image={cart.product.thumbnail}
+            alt={name}
+            sx={{ width: 100, height: 100, borderRadius: 2 }}
+          />
 
-      <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1, ml: 2 }}>
-        <CardContent sx={{ pb: "0 !important" }}>
-          <Typography variant="subtitle1">{name}</Typography>
-          <Typography variant="body2" color="text.secondary">
-            색상: {color} / 사이즈: {size}
-          </Typography>
-        </CardContent>
+          <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1, ml: 2 }}>
+            <CardContent sx={{ pb: "0 !important" }}>
+              <Typography variant="subtitle1">{name}</Typography>
+              <Typography variant="body2" color="text.secondary">
+                색상: {color} / 사이즈: {size}
+              </Typography>
+            </CardContent>
 
-        <Box sx={{ display: "flex", alignItems: "center", mt: 1, px: 2 }}>
-          <Button size="small" onClick={onDecrease}>-</Button>
-          <Typography sx={{ mx: 2 }}>{quantity}</Typography>
-          <Button size="small" onClick={onIncrease}>+</Button>
+            <Box sx={{ display: "flex", alignItems: "center", mt: 1, px: 2 }}>
+              <Button size="small" onClick={onDecrease}>-</Button>
+              <Typography sx={{ mx: 2 }}>{quantity}</Typography>
+              <Button size="small" onClick={onIncrease}>+</Button>
 
-          <Typography sx={{ ml: "auto" }}>
-            {(price * quantity).toLocaleString()}원
-          </Typography>
-        </Box>
-      </Box>
+              <Typography sx={{ ml: "auto" }}>
+                {(price * quantity).toLocaleString()}원
+              </Typography>
+            </Box>
+          </Box>
 
-      <IconButton onClick={onRemove}>
-        <DeleteIcon />
-      </IconButton>
-    </Card>
+          <IconButton onClick={onRemove}>
+            <DeleteIcon />
+          </IconButton>
+        </Card>
+      ))}
+    </>
   );
 }
--- frontend/src/pages/CartList.js
+++ frontend/src/pages/CartList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import CartItem from "../components/CartItem";
 
 export default function CartList() {
