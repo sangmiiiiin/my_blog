f60923b07c63ee06c22ca5ac906c5046c72646f3
sangmiiiiin <kwk680037@gmail.com>
2025-04-28 22:09:29 +0900

    💄 HeaderDrawer에 상품 카테고리 전체보기, 상의, 하의, 잡화 추가

--- frontend/src/components/HeaderDrawer.js
+++ frontend/src/components/HeaderDrawer.js
@@ -26,7 +26,8 @@ export default function TemporaryDrawer({ isAuthenticated }) {
     setOpen(false);
   }
   const afterLoginItems = '새 글 작성';
-  const itemList = ['로그인', '회원가입', '방명록'];
+  const userHandler = ['로그인', '회원가입', '방명록'];
+  const itemCategory = ['전체보기', '상의', '하의', '잡화']
 
   const AfterLoginRenderButtons = (text) => {
     return (
@@ -53,10 +54,13 @@ export default function TemporaryDrawer({ isAuthenticated }) {
             item === "회원가입" && handleNavigate("/register");
             item === "로그인" && handleNavigate("/login");
             item === "방명록" && handleNavigate("/main");
+            item === "상의" && handleNavigate("/shirt");
+            item === "하의" && handleNavigate("/pants");
+            item === "잡화" && handleNavigate("/etc");
           }}
         >
           <ListItemIcon>
-            {index % 2 === 0 ? <MailIcon/> : <InboxIcon/>}
+            {index % 2 === 0 ? <MailIcon /> : <InboxIcon />}
           </ListItemIcon>
           <ListItemText primary={item} />
         </ListItemButton>
@@ -68,7 +72,11 @@ export default function TemporaryDrawer({ isAuthenticated }) {
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
-        {isAuthenticated ? AfterLoginRenderButtons(afterLoginItems) : renderButtons(itemList)}
+        {isAuthenticated ? AfterLoginRenderButtons(afterLoginItems) : renderButtons(userHandler)}
+      </List>
+      <Divider />
+      <List>
+        {renderButtons(itemCategory)}
       </List>
       <Divider />
       <List>
